8be2db1a8748afe010c3f1ca7d271bcff61968b5
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-02-11 11:25:21 +0600

    Initialized database-auth for database authentication and authorization using JPA and spring security

---
+++ services/database-auth/src/main/java/com/nayem/databaseauth/DatabaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nayem.databaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseAuthApplication.class, args);
+	}
+
+}
