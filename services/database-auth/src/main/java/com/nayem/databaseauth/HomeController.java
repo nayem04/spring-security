3ae8d989c6b0a52984590a6826476501abdffcd9
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-02-11 11:51:52 +0600

    Added HomeController

---
+++ services/database-auth/src/main/java/com/nayem/databaseauth/HomeController.java
@@ -0,0 +1,24 @@
+package com.nayem.databaseauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class HomeController {
+    @GetMapping("home")
+    public String home() {
+        return "Welcome Home!";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
+}
