2fb89454abb1d9835cc0b427c9e13b12f1d0a5af
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-02-11 13:54:15 +0600

    Added a Router class for routing APIs

---
+++ services/database-auth/src/main/java/com/nayem/databaseauth/common/routing/Router.java
@@ -0,0 +1,26 @@
+package com.nayem.databaseauth.common.routing;
+
+public final class Router {
+    private static final String ROOT = "/api/";
+    private static final String VERSION1 = "v1/";
+
+    private static final String HOME = ROOT + VERSION1 + "home";
+    private static final String HELLO_USER = ROOT + VERSION1 + "hello-user";
+    private static final String HELLO_ADMIN = ROOT + VERSION1 + "hello-admin";
+
+    public static final class Role {
+        public static final String GET_ROLES = ROOT + VERSION1 + "roles";
+        public static final String GET_ROLE = ROOT + VERSION1 + "roles/{id}";
+        public static final String CREATE_ROLE = ROOT + VERSION1 + "roles";
+        public static final String UPDATE_ROLE = ROOT + VERSION1 + "roles/{id}";
+        public static final String DELETE_ROLE = ROOT + VERSION1 + "roles/{id}";
+    }
+
+    public static final class User {
+        public static final String GET_USERS = ROOT + VERSION1 + "users";
+        public static final String GET_USER = ROOT + VERSION1 + "users/{id}";
+        public static final String CREATE_USER = ROOT + VERSION1 + "users";
+        public static final String UPDATE_USER = ROOT + VERSION1 + "users/{id}";
+        public static final String DELETE_USER = ROOT + VERSION1 + "users/{id}";
+    }
+}
