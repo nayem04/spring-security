5d5c4c407e1f2b23cd22c057b1d806ca10588528
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-02-11 14:38:27 +0600

    Mapped role to users

--- services/database-auth/src/main/java/com/nayem/databaseauth/domain/role/entities/Role.java
+++ services/database-auth/src/main/java/com/nayem/databaseauth/domain/role/entities/Role.java
@@ -1,12 +1,16 @@
 package com.nayem.databaseauth.domain.role.entities;
 
 import com.nayem.databaseauth.common.base.BaseEntity;
+import com.nayem.databaseauth.domain.user.entities.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
@@ -22,4 +26,7 @@ public class Role extends BaseEntity {
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 }
